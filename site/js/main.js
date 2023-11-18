9f3afdd63363ac50a6be29fcf7816aa4e40f0585
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2023-11-18 16:52:01 -0300

    18.11.2023

--- site/js/main.js
+++ site/js/main.js
@@ -84,6 +84,20 @@ function abrirModal(cardId){
     modal.classList.remove('none')
 }
 
+<<<<<<< HEAD
+=======
+function fecharModal(cardId) {
+    const card = document.querySelector(`#${cardId}`)
+    const fade = card.querySelector('#fade')
+    const modal = card.querySelector('#modal')
+    const videoIframe = card.querySelector('#video')
+    fade.classList.add('none')
+    modal.classList.add('none')
+    
+    pausarVideo(videoIframe)
+}
+
+>>>>>>> parent of c3377a2 (dasdsa)
 function pausarVideo(video) {
     const videoURL = video.src;
     video.src = videoURL;
